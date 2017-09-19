6e3c3b25256a6bcdd82cca09816cd59e2aeb9804
DESKTOP-8TCPMCB\Liem Phan <minhliemphp@gmail.com>
2017-09-19 21:15:54 +0700

    First Commit

---
+++ src/ModulesServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ModulesServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+      $this->publishes([__DIR__.'/config/module.php' => config_path('module.php')], 'module_config');
+      $this->publishes([__DIR__.'/Events' => base_path('app/Events')], 'module_event');
+      $this->publishes([__DIR__.'/Modules' => base_path('app/Modules')], 'module');
+      // $this->publishes([__DIR__.'/Repositories' => base_path('app/Repositories')], 'module_respository');
+      $this->publishes([__DIR__.'/resources/assets' => public_path('/assets')], 'module_assets');
+      // $this->publishes([__DIR__.'/resources/myLib' => base_path('/resources')], 'module_library');
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
